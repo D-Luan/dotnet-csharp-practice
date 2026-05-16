76a3c6e0699f78c99c94ed9409d2dadb711f9bc7
d-luan <dluan1285@gmail.com>
2026-05-16 02:43:33 -0300

    feat: add product and stock system exercise

---
+++ fundamentals/01CsharpBasics/ProductAndStockExercise/Product.cs
@@ -0,0 +1,55 @@
+﻿namespace ProductAndStockExercise;
+
+public class Product
+{
+    public string Name { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
+    public int QuantityInStock { get; private set; }
+
+    public Product(string name, decimal price, int quantityInStock)
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name cannot be empty");
+
+        if (price <= 0) throw new ArgumentException("Price cannot be less or equal than 0");
+
+        if (quantityInStock < 0) throw new ArgumentException("Quantity in Stock cannot be negative");
+
+        Name = name;
+        Price = price;
+        QuantityInStock = quantityInStock;
+    }
+
+    public void AddStock(int quantity)
+    {
+        if (quantity <= 0) throw new ArgumentException("Quantity must be greater than 0");
+        QuantityInStock += quantity;
+    }
+
+    public void RemoveStock(int quantity)
+    {
+        if (quantity <= 0) throw new ArgumentException("Quantity must be greater than 0");
+
+        if (quantity > QuantityInStock) throw new ArgumentException("Quantity cannot be greater than stock");
+
+        QuantityInStock -= quantity;
+    }
+
+    public decimal GetTotalValueInStock() => Price * QuantityInStock;
+
+    public bool IsAvailable() => QuantityInStock > 0;
+
+    public void ChangePrice(decimal newPrice)
+    {
+        if (newPrice <= 0) throw new ArgumentException("New price must be greater than 0");
+        Price = newPrice;
+    }
+
+    public void DisplayData()
+    {
+        Console.WriteLine($"Product: {Name}");
+        Console.WriteLine($"Price: R$ {Price:F2}");
+        Console.WriteLine($"Quantity in stock: {QuantityInStock}");
+        Console.WriteLine($"Available: {(IsAvailable() ? "Yes" : "No")}");
+        Console.WriteLine($"Total value in stock: R$ {GetTotalValueInStock():F2}");
+    }
+}
---
+++ fundamentals/01CsharpBasics/ProductAndStockExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using ProductAndStockExercise;
+
+Product product = new Product("Playstation 4", 1200, 50);
+product.DisplayData();
+Console.WriteLine();
+
+product.AddStock(5);
+product.RemoveStock(3);
+product.DisplayData();
+Console.WriteLine();
+
+Console.WriteLine($"Product is available? {(product.IsAvailable() ? "Yes" : "No")}");
+Console.WriteLine($"Total value in stock: R$ {product.GetTotalValueInStock():F2}");
+
+try
+{
+    product.AddStock(-33);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+try
+{
+    product.RemoveStock(0);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+Console.WriteLine();
+
+product.ChangePrice(2000);
+product.DisplayData();
